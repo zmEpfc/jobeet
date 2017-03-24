a9777d2d2f6d2b7e7e0ae370768434d707d608d5
zmEpfc <zagai.mehdi@gmail.com>
2017-03-24 17:19:23 +0100

    Réalisation du formulaire d'upload

--- src/Ens/JobeetBundle/Form/JobType.php
+++ src/Ens/JobeetBundle/Form/JobType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 
 class JobType extends AbstractType
@@ -20,12 +21,12 @@ class JobType extends AbstractType
         $builder->add('type', TextType::class, array('label' => 'Type'));
         $builder->add('company', TextType::class, array('label' => 'Compagnie'));
         $builder->add('logo', TextType::class, array('label' => 'Logo'));
+        $builder->add('file', FileType::class, array('label' => 'Company logo', 'required' => false));
         $builder->add('url', TextType::class, array('label' => 'Url'));
         $builder->add('position', TextType::class, array('label' => 'Position'));
         $builder->add('location', TextType::class, array('label' => 'Location'));
         $builder->add('description', TextType::class, array('label' => 'Description'));
         $builder->add('how_to_apply', TextType::class, array('label' => 'Comment postuler'));
-        $builder->add('token');
         $builder->add('is_public', TextType::class, array('label' => 'est publique ?'));
         $builder->add('email', TextType::class, array('label' => 'e-mail'));
         $builder->add('type', ChoiceType::class, array('choices' => Job::getTypes(), 'expanded' => true));
